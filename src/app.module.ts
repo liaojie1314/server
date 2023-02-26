81c503d62025ba99142e545dd80bd2509dd29e1a
liaojie1314 <1517438366@qq.com>
2023-02-26 14:51:04 +0800

    project architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './common/database/database.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [UserModule,DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
